02d32246adea34c38f2494a532a06cc9ab65c55e
djapothecary <39279740+djapothecary@users.noreply.github.com>
2019-03-20 01:54:24 -0600

    fixed file structures

--- IguanaDenStudios/iguanadenstudios/home/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, redirect, url_for
-from iguanadenstudios import db # may not be needed
-
-home_blueprint = Blueprint('home', __name__,
-                            template_folder = 'templates/home')
-
-@home_blueprint.route('/index')
-def index():
-    return render_template('index.html')
--- IguanaDenStudios/iguanadenstudios/mastering/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, redirect, url_for
-from iguanadenstudios import db # may not be needed
-
-mastering_blueprint = Blueprint('mastering', __name__,
-                                template_folder = 'templates/mastering')
-
-@mastering_blueprint.route('/mastering')
-def mastering():
-    return render_template('mastering.html')
