fa052c770b23c98f629e7668b199928dfcb46063
djapothecary <39279740+djapothecary@users.noreply.github.com>
2019-03-26 12:16:43 -0600

    still working on tracklist display

--- iguanadenstudios/tracklists/routes.py
+++ iguanadenstudios/tracklists/routes.py
@@ -13,27 +13,6 @@ def tracklists():
                             artist_tracklist_name = artist_tracklist_name)
 
 @tracklists_blueprint.route('/<string:artist_dj_name>')
-def load_tracklist_display(artist_dj_name):
-    # tracklist_details = [TracklistDetails.query.get(tracklist_name_id)]
-    #tracklist_details = [TracklistDetails.query.get(artist_dj_name)]
-    tracklist_details = TracklistDetails.query.outerjoin(TracklistName).all()
-    td_list = []
-
-    for item in tracklist_details:
-        td_list.append(item)
-
-    #tracklist_details = TracklistDetails.query(TracklistDetails).filter_by(id = tracklist_details_id).first()
-
-    artist_tracklist_name = TracklistName.query.group_by('artist_dj_name').all()
-
-
-    return render_template('tracklists.html',
-                            # track_artist = tracklist_details.track_artist,
-                            # track_title = tracklist_details.track_title,
-                            artist_tracklist_name = artist_tracklist_name,
-                            td_list = td_list)
-
-@tracklists_blueprint.route('/<string:artist_dj_name>')
 def load_tracklist(artist_dj_name):
     # tracklist_details = [TracklistDetails.query.get(tracklist_name_id)]
     #tracklist_details = [TracklistDetails.query.get(artist_dj_name)]
@@ -53,3 +32,24 @@ def load_tracklist(artist_dj_name):
                             # track_title = tracklist_details.track_title,
                             artist_tracklist_name = artist_tracklist_name,
                             td_list = td_list)
+
+# @tracklists_blueprint.route('/<string:artist_dj_name>')
+# def load_tracklist_display(artist_dj_name):
+#     # tracklist_details = [TracklistDetails.query.get(tracklist_name_id)]
+#     #tracklist_details = [TracklistDetails.query.get(artist_dj_name)]
+#     tracklist_details = TracklistDetails.query.outerjoin(TracklistName).all()
+#     td_list = []
+
+#     for item in tracklist_details:
+#         td_list.append(item)
+
+#     #tracklist_details = TracklistDetails.query(TracklistDetails).filter_by(id = tracklist_details_id).first()
+
+#     artist_tracklist_name = TracklistName.query.group_by('artist_dj_name').all()
+
+
+#     return render_template('tracklists.html',
+#                             # track_artist = tracklist_details.track_artist,
+#                             # track_title = tracklist_details.track_title,
+#                             artist_tracklist_name = artist_tracklist_name,
+#                             td_list = td_list)
