775de073cab0969eda1af9d4c08f670101aa31c5
djapothecary@gmail.com <39279740+djapothecary@users.noreply.github.com>
2020-02-17 08:41:39 -0700

    updated application factory methods

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,15 @@
-import os
 from iguanadenstudios import create_app, db
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'dev')
+app = create_app()
+
+@app.before_first_request
+def create_tables():
+    import pdb; pdb.set_trace()
+    if app.config['ENV'] =='development':
+        import pdb; pdb.set_trace()
+        db.create_all()
+    else:
+        db.create_all()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
