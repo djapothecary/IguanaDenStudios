9b6a947ebc61d9137246eda4722667526de05859
djapothecary@gmail.com <39279740+djapothecary@users.noreply.github.com>
2020-02-17 13:17:01 -0700

    remove argument parser

--- wsgi.py
+++ wsgi.py
@@ -1,44 +1,46 @@
 from iguanadenstudios import create_app, db
 from flask_migrate import Migrate
-from argparse import ArgumentParser
+# from argparse import ArgumentParser
 
-parser = ArgumentParser(description = 'Configure what settings to use')
+# parser = ArgumentParser(description = 'Configure what settings to use')
 
-parser.add_argument("-d",
-                    "--dev",
-                    required = False,
-                    type = str,
-                    dest = 'dev',
-                    metavar = 'dev',
-                    help = 'run the application as a developer')
-parser.add_argument('-t',
-                    '--test',
-                    required = False,
-                    type = str,
-                    dest = 'test',
-                    metavar = 'test',
-                    help = 'run the application as a tester')
-parser.add_argument('-p',
-                    '--prod',
-                    required = False,
-                    type = str,
-                    dest = 'prod',
-                    metavar = 'prod',
-                    help = 'run the application in production')
-args = parser.parse_args()
+# parser.add_argument("-d",
+#                     "--dev",
+#                     required = False,
+#                     type = str,
+#                     dest = 'dev',
+#                     metavar = 'dev',
+#                     help = 'run the application as a developer')
+# parser.add_argument('-t',
+#                     '--test',
+#                     required = False,
+#                     type = str,
+#                     dest = 'test',
+#                     metavar = 'test',
+#                     help = 'run the application as a tester')
+# parser.add_argument('-p',
+#                     '--prod',
+#                     required = False,
+#                     type = str,
+#                     dest = 'prod',
+#                     metavar = 'prod',
+#                     help = 'run the application in production')
+# args = parser.parse_args()
 
 
-if args.dev or args.test is not None:
-    print('running dev configuration')
-    app = create_app('dev')
-elif args.prod is not None:
-    print('running prod configuration')
-    app = create_app('prod')
-else:
-    app = create_app('dev')
+# if args.dev or args.test is not None:
+#     print('running dev configuration')
+#     app = create_app('dev')
+# elif args.prod is not None:
+#     print('running prod configuration')
+#     app = create_app('prod')
+# else:
+#     app = create_app('dev')
 
+# migrate = Migrate(app, db)
+
+app = create_app('dev')
 migrate = Migrate(app, db)
-
 #app = create_app()
 
 @app.before_first_request
